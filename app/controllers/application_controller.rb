fe971dd2fb0fd1181569a6b51227a3be58a733e1
amanbanduni05 <aman@clanconnect.ai>
2021-04-09 10:30:34 +0530

    Add session controller , login and current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-	def hello
-		render html: "Hello, World!"
-	end
+	# def hello
+	# 	render html: "Hello, World!"
+	# end
+	include SessionsHelper
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get '/sign_up' , to: 'users#new'
-  root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
+	root 'static_pages#home'
+	get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  get '/sign_up' , to: 'users#new'
+  get '/login',	to: 'sessions#new'
+	post '/login', to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
+
   resources :users
 end
